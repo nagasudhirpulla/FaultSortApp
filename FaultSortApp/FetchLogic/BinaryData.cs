e9e6040098bd2411f4e2fffc91c1c0f80b45ceae
perf1 <perf1@rwrldc.urt.in>
2018-07-16 14:42:27 +0530

    boiler plate

---
+++ FaultSortApp/FetchLogic/BinaryData.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace FaultSortApp
+{
+    public enum DataQuality
+    {
+        Good,
+        Suspect,
+        Garbage,
+        Replaced
+    };
+
+    public enum DataQualityReason
+    {
+        None,
+        PMUSync,
+        PMUError
+    };
+
+    [Serializable]
+    public class PMUDataStructure
+    {
+        public DateTime TimeStamp { get; set; }
+        public float[] Value { get; set; }
+        public DataQuality Quality { get; set; }
+        public DataQualityReason QualityReason { get; set; }
+
+        public PMUDataStructure(DateTime dateTime, float value1, float value2)
+        {
+            TimeStamp = dateTime;
+            Value = new float[2] { value1, value2 };
+        }
+
+        public PMUDataStructure(DateTime dateTime, float value)
+        {
+            TimeStamp = dateTime;
+            Value = new float[1] { value };
+        }
+
+        public PMUDataStructure(DateTime dateTime, float[] value, DataQuality quality = DataQuality.Good, DataQualityReason qualityReason = DataQualityReason.None)
+        {
+            TimeStamp = dateTime;
+            Value = value;
+            Quality = quality;
+            QualityReason = qualityReason;
+        }
+
+        public PMUDataStructure()
+        {
+        }
+    }    
+}
---
+++ notes/data_fetch.md
@@ -0,0 +1,13 @@
+## Files required for data fetch
+
+PhasorPointBinaryDataParser.cs
+
+PhasorPointEndpointBehavior.cs
+
+PhasorPointMessageInspector.cs
+
+BinaryData.cs
+
+HistoryDataAdapter.cs
+
+ConfigurationManager.cs
