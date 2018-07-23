fc338bed48f79bcbfae1ae5f62f975d8fb001917
perf1 <perf1@rwrldc.urt.in>
2018-07-23 11:12:04 +0530

    added files

---
+++ FaultSortApp/FetchLogic/ConfigManager.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace FaultSortApp.FetchLogic
+{
+    public class ConfigManager
+    {
+        public int LatencyTime { get; set; }
+
+        public string Host { get; set; }
+
+        public int Port { get; set; }
+
+        public string Path { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public void Initialize()
+        {
+            XmlDocument domDocument = new XmlDocument();
+
+            string path = Environment.CurrentDirectory;
+            string xmlPath = path + "\\AppSettings.xml";
+
+            if (File.Exists(xmlPath))
+            {
+                domDocument.Load(xmlPath);
+                XmlNode node = domDocument.SelectSingleNode("HistoryDataProvider/DataSource");
+
+                Host = node.Attributes.GetNamedItem("Host").Value;
+                string port = node.Attributes.GetNamedItem("Port").Value;
+                Port = Int32.Parse(port);
+                Path = node.Attributes.GetNamedItem("Path").Value;
+                UserName = node.Attributes.GetNamedItem("UserName").Value;
+                Password = node.Attributes.GetNamedItem("Password").Value;
+
+                LatencyTime = 1;
+
+                XmlNode latencyNode = domDocument.SelectSingleNode("HistoryDataProvider/LatencyTime");
+                if (latencyNode != null)
+                {
+                    string latencyValue = latencyNode.Value;
+                    int latency = 1;
+                    if (int.TryParse(latencyValue, out latency))
+                        LatencyTime = latency;
+                }
+            }
+            else
+            {
+                Host = "172.16.183.131";
+                Port = 24721;
+                Path = "/eterra-ws/HistoricalTrendProvider";
+                UserName = "pdcAdmin";
+                Password = "p@ssw0rd";
+                LatencyTime = 1;
+            }
+        }
+
+        internal void Save()
+        {
+            var sw = new StringWriter(new StringBuilder(1024));
+            var xmlTextWriter = new XmlTextWriter(sw);
+            xmlTextWriter.Formatting = Formatting.Indented;
+
+            xmlTextWriter.WriteStartElement("HistoryDataProvider");
+            xmlTextWriter.WriteStartElement("DataSource");
+
+            xmlTextWriter.WriteStartAttribute("Host");
+            xmlTextWriter.WriteString(Host);
+            xmlTextWriter.WriteEndAttribute();
+
+            xmlTextWriter.WriteStartAttribute("Port");
+            xmlTextWriter.WriteString(this.Port.ToString());
+            xmlTextWriter.WriteEndAttribute();
+
+            xmlTextWriter.WriteStartAttribute("Path");
+            xmlTextWriter.WriteString(this.Path);
+            xmlTextWriter.WriteEndAttribute();
+
+            xmlTextWriter.WriteStartAttribute("UserName");
+            xmlTextWriter.WriteString(this.UserName);
+            xmlTextWriter.WriteEndAttribute();
+
+            xmlTextWriter.WriteStartAttribute("Password");
+            xmlTextWriter.WriteString(this.Password);
+            xmlTextWriter.WriteEndAttribute();
+
+            xmlTextWriter.WriteEndElement();
+
+            xmlTextWriter.WriteStartElement("LatencyTime");
+            xmlTextWriter.WriteString(LatencyTime.ToString());
+            xmlTextWriter.WriteEndElement();
+
+            xmlTextWriter.WriteEndElement();
+            xmlTextWriter.Close();
+
+            string path = Environment.CurrentDirectory;
+            string xmlPath = path + "\\AppSettings.xml";
+
+            File.WriteAllText(xmlPath, sw.ToString());
+        }
+
+    }
+}
---
+++ FaultSortApp/Utils/ExcelUtils.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * Writing data to excel
+ * https://stackoverflow.com/questions/23041021/how-to-write-some-data-to-excel-file-xlsx
+ * 
+ * Find number of elements in a tuple
+ * https://stackoverflow.com/questions/7631327/number-of-elements-in-tuple
+ */
+namespace FaultSortApp.Utils
+{
+    public class ExcelUtils
+    {
+        public static void DumpCurrRatioResultsToExcel(List<Tuple<double, string, DateTime>> currRatios, string fName)
+        {
+            //dump values to excel
+            Microsoft.Office.Interop.Excel.Application oXL;
+            Microsoft.Office.Interop.Excel._Workbook oWB;
+            Microsoft.Office.Interop.Excel._Worksheet oSheet;
+
+            //Start Excel and get Application object.
+            oXL = new Microsoft.Office.Interop.Excel.Application();
+            oXL.Visible = true;
+
+            //Get a new workbook.
+            oWB = (oXL.Workbooks.Add(""));
+            oSheet = oWB.ActiveSheet;
+
+            // Iterate through list and dump in excel
+            for (int tupleIter = 0; tupleIter < currRatios.Count; tupleIter++)
+            {
+                oSheet.Cells[tupleIter + 1, 1] = currRatios[tupleIter].Item2;
+                oSheet.Cells[tupleIter + 1, 2] = currRatios[tupleIter].Item1;
+                oSheet.Cells[tupleIter + 1, 3] = currRatios[tupleIter].Item3;
+            }
+
+            oXL.Visible = false;
+            oXL.UserControl = false;
+            oWB.SaveAs(fName, Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookDefault, Type.Missing, Type.Missing,
+                false, false, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlNoChange,
+                Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+
+            oWB.Close();
+        }
+    }
+}
